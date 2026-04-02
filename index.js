c5d060d758ac289aeb970640da3a9cf55a6e273f
MH-liton-360 <liton72524nk@gmail.com>
2026-04-02 20:56:07 +0600

    adding middile ware and calling express

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+// call express 
+const app = express();
+
+// middileWare 
+app.use(cors());
+app.use(express.json());
