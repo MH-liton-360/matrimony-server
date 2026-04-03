f1e343f2263d13c3639d53331faacb65dd0cb441
MH-liton-360 <liton72524nk@gmail.com>
2026-04-03 09:59:26 +0600

    connected successfully mongodb

--- index.js
+++ index.js
@@ -9,13 +9,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-// users +  Password 
-// matrimonyW2
-// PQxP2wL7QBgMEjsq
 
-
-
-const uri = "mongodb+srv://matrimonyW2:PQxP2wL7QBgMEjsq@cluster0.uru7rsz.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uru7rsz.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
