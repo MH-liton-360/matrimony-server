6b808d4fbf276bebc163c36e466bb879cd0f1d77
MH-liton-360 <liton72524nk@gmail.com>
2026-04-03 09:23:47 +0600

    port replace

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // call express 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // middileWare 
 app.use(cors());
