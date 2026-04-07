c8f1ef7f87f7e214d508c4802136d7eb046a9979
MH-liton-360 <liton72524nk@gmail.com>
2026-04-07 21:12:45 +0600

    search api add

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-// Middle Ware 
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uru7rsz.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -28,52 +25,63 @@ async function run() {
         const database = client.db("matrimonyDB");
         const biodataCollection = database.collection("biodata");
 
-        // routes
+        //  Create Biodata
         app.post('/api/biodata', async (req, res) => {
             const data = req.body;
             const result = await biodataCollection.insertOne(data);
             res.send(result);
         });
 
+        //  Get ALL Biodata (Search use korbe)
         app.get('/api/biodata', async (req, res) => {
-            try {
-                const specialProfessions = ["doctor", "professor", "engineer", "actor", "sportsman"];
+            const result = await biodataCollection.find().toArray();
+            res.send(result);
+        });
 
-                const result = await biodataCollection.aggregate([
-                    {
-                        $addFields: {
-                            priority: {
-                                $cond: [
-                                    { $in: [{ $toLower: "$profession" }, specialProfessions] },
-                                    1,
-                                    2
-                                ]
-                            }
+        //  Featured Premium
+        app.get('/api/biodata/featured', async (req, res) => {
+            const specialProfessions = ["doctor", "professor", "engineer", "actor", "sportsman"];
+
+            const result = await biodataCollection.aggregate([
+                {
+                    $addFields: {
+                        priority: {
+                            $cond: [
+                                { $in: [{ $toLower: "$profession" }, specialProfessions] },
+                                1,
+                                2
+                            ]
                         }
-                    },
-                    { $sort: { priority: 1 } },
-                    { $limit: 4 }
-                ]).toArray();
+                    }
+                },
+                { $sort: { priority: 1 } },
+                { $limit: 4 }
+            ]).toArray();
 
-                res.send(result);
-            } catch (err) {
-                console.error(err);
-                res.status(500).send({ error: "Something went wrong" });
-            }
+            res.send(result);
         });
 
+        // Search API
+        app.get('/api/biodata/search', async (req, res) => {
+            const { age, profession, district } = req.query;
 
-        // Biodata details page get api
+            let query = {};
+
+            if (age) query.age = Number(age);
+            if (profession) query.profession = { $regex: profession, $options: "i" };
+            if (district) query.district = { $regex: district, $options: "i" };
+
+            const result = await biodataCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // Single biodata
         app.get("/api/biodata/:id", async (req, res) => {
-            try {
-                const { id } = req.params;
-                const result = await biodataCollection.findOne({ _id: new ObjectId(id) });
-                res.send(result);
-            } catch (err) {
-                console.error(err);
-                res.status(500).send({ error: "Something went wrong" });
-            }
+            const { id } = req.params;
+            const result = await biodataCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
         });
+
         console.log("MongoDB Connected.");
 
     } catch (err) {
@@ -88,4 +96,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Matrimony is running on port: ${port}`);
-})
+});
